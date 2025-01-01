16002974361b683479259328523a328a4ec345ba
疯狂的大叔 <1392429292@qq.com>
2025-01-01 19:51:46 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+
 # 默认ip
 uci set network.lan.ipaddr='10.0.0.1'
+
+
+# 配置WAN口为PPPoE拨号模式
+uci set network.wan.proto='pppoe'
+uci set network.wan.username='02188888888'
+uci set network.wan.password='888888'
+uci set network.wan.ifname='eth1'
+
+
+# 提交并应用配置
+uci commit network
+
 
 # 退出状态码
 exit 0
